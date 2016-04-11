5aa21d8112a8ac4f7678da98cf68a263fa428f22
Karl Steltenpohl <karl.steltenpohl@superflypresents.com>
2016-04-11 14:13:10 -0500

    favicon, deploy script, etc...

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bucket Id for production: botmap.io
-aws s3 sync ./dist s3://botmap.io --region="us-east-1" --exclude="Gruntfile.js" --exclude="package.json" --exclude="README.md" --exclude="bower.json" --exclude=".gitignore" --exclude=".DS_Store" --exclude="deploy.sh" --exclude=".git/*" --exclude="_sources/*" --exclude=".sass-cache/*" --exclude=".tmp/*" --exclude="node_modules/*"
+grunt build
+aws s3 cp ./dist s3://botmap.io --region="us-east-1" --acl public-read --recursive
